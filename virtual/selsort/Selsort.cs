491f6e2e2ce7e86de22caa744841f75b2ad612b4
emno <emno@itu.dk>
2023-11-02 11:25:54 +0100

    compile and dissas on java variant

--- virtual/selsort/Selsort.cs
+++ virtual/selsort/Selsort.cs
@@ -18,12 +18,14 @@ class Selsort {
   public static readonly Random rnd = new Random(); 
 
   public static void SelectionSort(int[] arr) {
-    for (int i = 0; i < arr.Length; i++) {
-      int least = i;                                      
-      for (int j = i+1; j < arr.Length; j++)
-	if (arr[j] < arr[least])
-	  least = j;
-      int tmp = arr[i]; arr[i] = arr[least]; arr[least] = tmp;
-    }
+  for (int i = 0; i < arr.Length; i++) {
+    int least = i;                                      
+    for (int j = i+1; j < arr.Length; j++)
+      if (arr[j] < arr[least])
+        least = j;
+    int tmp = arr[i]; 
+    arr[i] = arr[least]; 
+    arr[least] = tmp;
+  }
   }
 }
